7db271b4128a6d42fbd83e428f74143dfd41549d
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-20 09:38:26 +0300

    server

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+//-------------------------
+//MongoDB Atlas
+const URI = "mongodb+srv://only4Me:only4MeDB@cluster0-ztzuu.mongodb.net/pranayama?retryWrites=true&w=majority";
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", function () {
+  console.log("MongoDB connected successfully");
+});
+
+//------------------------
+//SERVER
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
