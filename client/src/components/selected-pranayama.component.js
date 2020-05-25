a84d2151e4f3c09107196a957525f8e46faa5d4f
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-25 17:09:58 +0300

    sounds

--- client/src/components/selected-pranayama.component.js
+++ client/src/components/selected-pranayama.component.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import sound from "../sounds/bhastrika.m4a";
+
 // import { Link } from "react-router-dom";
 export default class SelectedPranayama extends Component {
   // step 1 --------------------
@@ -32,6 +36,9 @@ export default class SelectedPranayama extends Component {
         <h1>Selected Pranayama: </h1>
         <h3>{this.state.name}</h3>
         <h4>{this.state.restriction ? "Yes" : "No"}</h4>
+        {/* <h4>{(src = `./sounds/${this.state.name}.m4a`)}</h4> */}
+        <AudioPlayer src={sound} onPlay={(e) => console.log("onPlay")} />
+        {/* <AudioPlayer src="./sounds/${this.state.name}.m4a" onPlay={(e) => console.log("onPlay")} /> */}
       </div>
     );
   }
